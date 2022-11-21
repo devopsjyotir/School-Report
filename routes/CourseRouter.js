dbe7653c853b87107dcadaabfbc423dce6c663b8
Tyler <tylercaillet2121@gmail.com>
2022-11-21 17:18:04 -0500

    routes updated

--- routes/CourseRouter.js
+++ routes/CourseRouter.js
@@ -5,7 +5,7 @@ const controller = require('../controllers/CoursesController')
 Router.get('/',controller.GetAllCourses)
 Router.get('/:course_id',controller.GetCourseDetails)
 Router.get('/course/:student_id',controller.GetStudentCourses)
-Router.post('/create',controller.CreateCourse)
+Router.post('/',controller.CreateCourse)
 Router.put('/:course_id',controller.UpdateCourse)
 Router.delete('/:course_id',controller.DeleteCourse)
 
