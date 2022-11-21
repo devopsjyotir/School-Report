8f45ca1f84b9e4da50a6a1f5ade57e9f09af59a2
Tyler <tylercaillet2121@gmail.com>
2022-11-21 17:31:32 -0500

    updates

--- seeders/20221121220855-studentcoursegrade.js
+++ seeders/20221121220855-studentcoursegrade.js
@@ -3,23 +3,22 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    const studentcoursegrade = [...Array(20)].map((_) => {
+      return {
+     
+        course_id: falso.randNumber({min:1,max:20}),
+        grade_id: falso.randNumber({min:1,max:20}),
+        student_id: falso.randNumber({min:1,max:20}),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    })
+    await queryInterface.bulkInsert('studentcoursegrade',studentcoursegrade)
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+
+
+    await queryInterface.bulkDelete('studentcoursegrade');
   }
 };
