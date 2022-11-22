169c9770a9b84422008489294589993c3bbc5a6d
Fredy Laksmono <fredy.laksmono@gmail.com>
2022-11-22 07:22:07 -0600

    Add: Get student relation function

--- controllers/StudentsCoursesGradesController.js
+++ controllers/StudentsCoursesGradesController.js
@@ -1,4 +1,4 @@
-const { StudentsCoursesGrades } = require("../models");
+const { StudentsCoursesGrades, Course, Student, Grade } = require("../models");
 
 const GetAllRelations = async (req, res) => {
   try {
@@ -17,10 +17,44 @@ const CreateRelations = async (req, res) => {
     throw error;
   }
 };
-// provided studentId & courseId & gradeId -> Create new StudentsCoursesGrades with value for studentId & courseId & gradeId populated
-// provided studentId & courseId -> Create new StudentsCoursesGrades with value for studentId & courseId populated
+
+const GetStudentRelations = async (req, res) => {
+  try {
+    let result = [];
+    let studentId = parseInt(req.params.student_id);
+    const studentRelation = await Student.findAll({
+      where: {
+        id: studentId
+      },
+      attributes: ["id", "name"],
+      include: [
+        {
+          model: Course,
+          as: "course",
+          attributes: ["id", "name"],
+          include: [
+            {
+              model: Grade,
+              as: "courseGrades",
+              attributes: ["id"]
+            }
+          ]
+        }
+      ]
+    });
+    // for (let i = 0; i<studentRelation.length;i++){
+    //     let tempData = {
+
+    //     }
+    // }
+    res.send(studentRelation);
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = {
   GetAllRelations,
-  CreateRelations
+  CreateRelations,
+  GetStudentRelations
 };
--- routes/StudentCourseGradeRouter.js
+++ routes/StudentCourseGradeRouter.js
@@ -2,6 +2,7 @@ const Router = require("express").Router();
 const controller = require("../controllers/StudentsCoursesGradesController");
 
 Router.get("/", controller.GetAllRelations);
+Router.get("/:student_id", controller.GetStudentRelations);
 Router.post("/", controller.CreateRelations);
 
 module.exports = Router;
