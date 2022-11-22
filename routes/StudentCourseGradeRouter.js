0e78d2ea3e953f0eea767f8056808c4a188b9627
Fredy Laksmono <fredy.laksmono@gmail.com>
2022-11-22 06:16:14 -0600

    Add: create relation router

--- routes/StudentCourseGradeRouter.js
+++ routes/StudentCourseGradeRouter.js
@@ -2,5 +2,6 @@ const Router = require("express").Router();
 const controller = require("../controllers/StudentsCoursesGradesController");
 
 Router.get("/", controller.GetAllRelations);
+Router.post("/", controller.CreateRelations);
 
 module.exports = Router;
